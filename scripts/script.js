d0d3966c6ca74f734ccc74d7faefcdaa27e91d81
Raphael Macayan <111346262+manzs123@users.noreply.github.com>
2023-07-24 17:33:29 +0800

    Edit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const texts = [
+    "Developer",
+    "Gamer",
+    "Teacher",
+  ]; 
+  
+  let textIndex = 0;
+  let charIndex = 0;
+  const typingEffect = document.getElementById("type");
+  
+  function type() {
+    const currentText = texts[textIndex].slice(0, charIndex++);
+    typingEffect.textContent = currentText;
+  
+    if (charIndex > texts[textIndex].length) {
+      
+      textIndex = (textIndex + 1) % texts.length;
+      charIndex = 0;
+    }
+  
+    setTimeout(type, 100); 
+  }
+  
+  
+  document.addEventListener("DOMContentLoaded", type);
+  
