81049febe3409e60ca77515012767705385f5e35
Denise <48487166+DeniseIvy@users.noreply.github.com>
2023-08-02 15:11:21 +0800

    cancelModal test

--- scripts/script.js
+++ scripts/script.js
@@ -34,4 +34,19 @@ document.addEventListener("DOMContentLoaded", function() {
       window.location.href = linkToOpen;
     });
 });
+
+var modal = document.getElementById("cancelModal");
+var btn = document.getElementById("btnCancel");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   
